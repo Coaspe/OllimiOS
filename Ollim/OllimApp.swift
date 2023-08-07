02628dc489843a3d8c9940725d65c7d93eaf58dd
Coaspe <aspalt85@gmail.com>
2023-08-07 22:49:56 +0900

    Initial Commit

---
+++ Ollim/OllimApp.swift
@@ -0,0 +1,17 @@
+//
+//  OllimApp.swift
+//  Ollim
+//
+//  Created by 이우람 on 2023/08/07.
+//
+
+import SwiftUI
+
+@main
+struct OllimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
