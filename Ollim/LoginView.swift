107c4372e092d009df4ec66eee24027829bef78b
Coaspe <aspalt85@gmail.com>
2023-08-07 23:34:14 +0900

    Initial commit

---
+++ Ollim/LoginView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Ollim
+//
+//  Created by 이우람 on 2023/08/07.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        VStack {
+            Image("transparent")
+                .resizable()
+                .frame(width: 100, height: 100)
+        }
+        .padding()
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
